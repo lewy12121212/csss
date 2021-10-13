e8931b7421e4c14adfe4f0a181be552752ef62b0
luki981259 <59644074+luki981259@users.noreply.github.com>
2021-10-13 10:09:05 +0200

    Add display picture from camera

--- csss_react/src/panels/login_panel/face_recognition/WebCamera.js
+++ csss_react/src/panels/login_panel/face_recognition/WebCamera.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import Webcam from "react-webcam";
 
   const videoConstraints = {
@@ -11,12 +11,16 @@ function WebCamera(props) {
 
 
   const webcamRef = React.useRef(null);
+  const [imgSrc, setImgSrc] = useState(null);
 
   const capture = React.useCallback(
     () => {
       const imageSrc = webcamRef.current.getScreenshot();
+      setImgSrc(imageSrc);
+      //console.log(imageSrc);
+
     },
-    [webcamRef]
+    [webcamRef, setImgSrc]
   );
 
   return (
@@ -24,13 +28,21 @@ function WebCamera(props) {
       <Webcam
         audio={false}
         mirrored={true}
+        imageSmoothing={true}
         height={720}
         ref={webcamRef}
         screenshotFormat="image/jpeg"
         width={1280}
         videoConstraints={videoConstraints}
       />
+      
       <button onClick={capture}>Capture photo</button>
+      {imgSrc && (
+        <img
+          src={imgSrc}
+          alt="pic1"
+        />
+      )}
     </div>
   );
 };
