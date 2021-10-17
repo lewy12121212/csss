9bae94e9eb1e1abb341198f58d21688c7e36462b
Anna Foltyniewicz <foltyniewicz.anna@gmail.com>
2021-10-17 14:48:38 +0200

    Add textbox to generate from given string

--- csss_react/src/qr_generator/QrGenerator.js
+++ csss_react/src/qr_generator/QrGenerator.js
@@ -1,29 +1,36 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import * as QRCode from 'easyqrcodejs';
 
-
 class QrGenerator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.qrcode = React.createRef();
+    this.options = null;
+  }
 
-    constructor(props) {
-        super(props);
-        this.qrcode = React.createRef();
-    }
+  handleChange = e => {
+    this.options = {
+      text: e.target.value,
+      width: 200,
+      height: 200,
+      colorDark: "#000000",
+      colorLight: "#FFFFFF",
+    };
+  }
 
-    componentDidMount() {
-        // Options
-        var options = {
-            text: "https://github.com/ushelp/EasyQRCodeJS"
-        }
-        // Create new QRCode Object
-        new QRCode( this.qrcode.current, options);
-    }
-    render() {
-        return ( 
-        <div className = "App">
-          <div ref={this.qrcode}></div> 
-        </div>
-        );
-    }
+  handleOption = () => {
+    new QRCode( this.qrcode.current, this.options);
+  }
+
+  render() {
+    return ( 
+      <div className = "App">
+        <input type="text" onChange={this.handleChange} />
+        <input type="button" value="Generuj QR code" onClick={this.handleOption} />
+        <div ref={this.qrcode}></div> 
+      </div>
+    );
+  }
 }
 
 export default QrGenerator;
