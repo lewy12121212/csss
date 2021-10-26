5752d6553041c0c6c16aabae93e7a1dc90bcb745
mar99ta <56344087+mar99ta@users.noreply.github.com>
2021-10-26 14:35:56 +0200

    Add table facture

--- csss_react/src/pdf/Pdf.js
+++ csss_react/src/pdf/Pdf.js
@@ -1,9 +1,10 @@
-import '@progress/kendo-theme-material/dist/all.css';
+import "@progress/kendo-theme-bootstrap/dist/all.scss";
+
 import { useRef } from 'react';
 import './pdf.scss';
 
 import { Button } from "@progress/kendo-react-buttons";
-import { PDFExport, savePDF } from "@progress/kendo-react-pdf";
+import { PDFExport } from "@progress/kendo-react-pdf";
 
 
 function Pdf(props) {
@@ -17,10 +18,51 @@ function Pdf(props) {
     return (
       <div className='app-content'>
         <PDFExport ref={pdfExportComponent} paperSize="A4">
-            <h1>KendoReact PDF Processing</h1>
-            <p>This is an example of text that may be styled</p>
+        <table className="container">
+            
+            <table border>
+              <tr><th rowspan="7">CSSS</th></tr>
+              <tr><td className="table-active">Miejsce wystawienia</td></tr>
+              <tr><td>Poznań</td></tr>
+              <tr><td className="table-active">Data wystawienia</td></tr>
+              <tr><td>Data wystawienia</td></tr>
+              <tr><td className="table-active">Data wykonania usługi</td></tr>
+              <tr><td>Data wykonania usługi</td></tr>
+            </table>
+
+            <table><tr></tr></table>
+
+            <table>
+              <tr className="table-active"><th >Dane klienta</th><th>Dane firmy</th></tr>
+              <tr><td>FIRMA</td><td>Center System Super Support</td></tr>
+              <tr><td>NIP</td><td>NIP</td></tr>
+              <tr><td>ULICA</td><td>ULICA</td></tr>
+              <tr><td>MIASTO</td><td>Poznań</td></tr>
+            </table>
+
+            <table>
+              <tr><th colspan="5">Faktura</th></tr>
+              <tr className="table-active"><td>Id</td><td>Nazwa towaru lub usługi</td><td>Ilość</td><td>Cena</td><td>Suma</td></tr>
+              <tr><td>Id</td><td>Nazwa towaru lub usługi</td><td>Ilość</td><td>Cena</td><td>Suma</td></tr>
+              <tr><td>Id</td><td>Nazwa towaru lub usługi</td><td>Ilość</td><td>Cena</td><td>Suma</td></tr>
+              <tr><td>Id</td><td>Nazwa towaru lub usługi</td><td>Ilość</td><td>Cena</td><td>Suma</td></tr>
+              <tr><td>Id</td><td>Nazwa towaru lub usługi</td><td>Ilość</td><td>Cena</td><td>Suma</td></tr>
+              <tr><td colspan="3"></td><td>Razem</td><td>X</td></tr>
+            </table>
+
+            <table>
+              <tr><td>Do zapłaty:</td><td>X</td></tr>
+            </table>
+
+            <table>
+              <tr><th>Podpis klienta</th><th>Podpis firmy</th></tr>
+              <tr><td>X</td><td>X</td></tr>
+            </table>
+
+          </table>
+
             <div className="button-area">
-              <Button primary={true} onClick={handleExportWithComponent}>Export</Button>
+              <Button primary={true} onClick={handleExportWithComponent}>Save</Button>
             </div>
         </PDFExport>
       </div>
