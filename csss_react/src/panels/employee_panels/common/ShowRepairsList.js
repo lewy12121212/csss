b521785ffe8bb769a2c04e4f7a74fe139f7bb685
Eryk Lewandowski <lewy12121212@gmail.com>
2021-12-02 01:58:01 +0100

    Change link style for reset client pass. Add connect to endpoint in ShowRepairsList.

--- csss_react/src/panels/employee_panels/common/ShowRepairsList.js
+++ csss_react/src/panels/employee_panels/common/ShowRepairsList.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
+import axios from 'axios';
 import { NavLink } from 'react-router-dom';
+
+import { dbAddress } from '../../../dbCon';
 //import { Route, Switch } from 'react-router-dom';
 //import { useLocation } from "react-router-dom";
 //import ShowRepair from "./ShowRepair"
 //<NavLink className="" exact to={`${location}/1`}>Zlecenie 1</NavLink>
+function useDidMount() {
+  const didMountRef = useRef(true);
+  
+  useEffect(() => {
+    didMountRef.current = false;
+  }, []);
+  return didMountRef.current;
+};
 
 function ShowRepairsList(props) {
+  const didMount = useDidMount();
+  const [repairsTable, setRepairsTable] = useState([])
   //const location = props.path.pathname;
+
+
+  useEffect(() => {
+    if(didMount) {
+      console.log('mounted');
+      axios.get(`https://${dbAddress}:4000/employee/common/allRepairs`).then(response => {
+        setRepairsTable(response.data.data)
+        console.log(response.data.data)
+      }).catch(error => {
+  //
+      });
+    } else {
+      console.log('state updated');
+    }
+  }, [setRepairsTable, repairsTable, didMount]);
+
 
   return (
     <div className="">
@@ -15,6 +44,11 @@ function ShowRepairsList(props) {
         <NavLink className="" to="/Repairs/1" target="_blank" >Zlecenie 1</NavLink>
         <NavLink className="" to="/Repairs/2" target="_blank" >Zlecenie 2</NavLink>
       </div>
+      {repairsTable.map((row) => 
+        <div key={row.Id}>
+          {row.Id}
+        </div>
+      )}
       {/*<Switch>
         <Route path="/Repairs/:id" render={routerProps => <ShowRepair id={routerProps.match.params.id} />} />
       </Switch>*/}
