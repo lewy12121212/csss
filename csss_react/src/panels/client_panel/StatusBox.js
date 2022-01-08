7da7f7fcbf6695f6534e7f330c315bc626bba0f8
Eryk Lewandowski <lewy12121212@gmail.com>
2022-01-08 21:33:07 +0100

    FIX - color of status box in client panel.

--- csss_react/src/panels/client_panel/StatusBox.js
+++ csss_react/src/panels/client_panel/StatusBox.js
@@ -8,16 +8,18 @@ function StatusBox(props) {
   useEffect(() => {
     console.log(props.data)
     if(props.data.Status === "Przyjęte"){
-    setBoxStatusClass("black")
-    } else if(props.data.Status === "Naprawiane"){
-      setBoxStatusClass("blue")
-    } else if(props.data.Status === "Oczekujące"){
-      setBoxStatusClass("yellow")
-    } else if(props.data.Status === "Oczekujące na decyzje") {
-      setBoxStatusClass("red")
-    } else if(props.data.Status === "Zamknięte"){
       setBoxStatusClass("green")
-    }
+      } else if(props.data.Status === "Naprawiane"){
+        setBoxStatusClass("blue")
+      } else if(props.data.Status === "Oczekujące"){
+        setBoxStatusClass("yellow")
+      } else if(props.data.Status === "Oczekujące na decyzje") {
+        setBoxStatusClass("red")
+      } else if(props.data.Status === "Zamknięte"){
+        setBoxStatusClass("green")
+      } else if(props.data.Status === "Decyzja"){
+        setBoxStatusClass("pink")
+      }
   }, [setBoxStatusClass, props.data])
 
   return (
